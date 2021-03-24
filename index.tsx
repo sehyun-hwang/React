d93767bda8dc3462b5438a43a36ec463153cb0f4
sehyun-hwang <hwanghyun3@gmail.com>
2021-03-24 17:26:41 +0900

    2 files modified

--- index.tsx
+++ index.tsx
@@ -1,31 +1,36 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
 
-interface AppProps { }
+// @ts-ignore
+import { io } from "socket.io-client";
+
+interface AppProps {}
 interface AppState {
   name: string;
 }
+
+const socket = io("wss://proxy.hwangsehyun.com/webrtc-onvif");
 
 class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'React'
+      name: "React"
     };
+
+    setInterval(() => socket.send({}), 1000);
   }
 
   render() {
     return (
       <div>
         <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <p>Start editing to see some magic happen :)</p>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
